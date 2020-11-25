fb95ea8008810527a4845e9660ce878e7c9b412a
Andy Hui <dev.andyhui@gmail.com>
2020-11-25 11:46:41 +0800

    Add react-native-web for web support

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.box}>
+        <Text style={styles.text}>Hello, world!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  box: {padding: 10},
+  text: {fontWeight: 'bold'},
+});
+
+export default App;
