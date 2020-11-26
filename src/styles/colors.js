f3464b32e6d807eb688d9f21f98aa29f4500f969
Andy Hui <dev.andyhui@gmail.com>
2020-11-26 18:13:34 +0800

    Add brand colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,1 +1,13 @@
-export const background = '#000000';
+export const themeBackground = '#FFFFFF';
+
+// Brand's primary colors
+export const aiaRed = 'rgb(211, 17, 12)';
+export const green = 'rgb(241, 90, 34)';
+export const citrus = 'rgb(248, 152, 32)';
+export const brightRed = 'rgb(234,28,45)';
+
+// Brand's secondary colors
+export const lightGrey = 'rgb(209, 211, 212)';
+export const coolGrey6 = 'rgb(187, 188, 190)';
+export const coolGrey9 = 'rgb(166, 118, 117)';
+export const coolGrey10 = 'rgb(88, 89, 91)';
